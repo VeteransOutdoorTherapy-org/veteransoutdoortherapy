b53241763e5afcaa5cabe970ea82392e0e53139c
VeteransOutdoorTherapy <326161341+VeteransOutdoorTherapy@users.noreply.github.com>
2026-09-23 20:52:27 -0500

    coulter

--- src/lib/sponsors.ts
+++ src/lib/sponsors.ts
@@ -14,7 +14,7 @@ export const sponsorLogos = [
 	},
 	{
 		name: "Coulter Lake Guest Ranch & Outfitters",
-		image: `${sponsorUploads}/coulter-lake-guest-ranch.webp`,
+		image: `${sponsorUploads}/coulter-lake-guest-ranch.jpg,
 		featured: true,
 	},
 	{
